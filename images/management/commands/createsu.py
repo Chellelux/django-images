f3badce576a7cbbbaa71af7b8ddde4f817ff0e5c
Chellux <mrnainggie@tip.edu.ph>
2026-03-20 01:26:18 +0800

    Add createsuperuser for render deploy

---
+++ images/management/commands/createsu.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'Creates a superuser.'
+
+    def handle(self, *args, **options):
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser(
+                username='admin',
+                email='admin@example.com',
+                password='complexpassword123'
+            )
+            self.stdout.write(self.style.SUCCESS('Superuser has been created.'))
+        else:
+            self.stdout.write('Superuser already exists.')
